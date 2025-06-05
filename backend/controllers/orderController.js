5e32fbbb57c88c75580a48ed80f18d2cdeb1a7a6
Jarshan <jarshanjauffar@gmail.com>
2025-06-05 13:21:04 +0530

    Updated orderController, orderRoute

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,16 @@
 import orderModel from "../models/orderModel.js"
 import userModel from "../models/userModel.js"
+// import Stripe from 'stripe'   12:12
+
+
+
+//global variables
+// const currency = 'lkr'
+// const deliveryCharge = 100
+
+
+//Gateway initialize.................
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)  need to create stripe account
 
 // Placing orders using Cash on Delivery (COD) method
 
@@ -30,15 +41,85 @@ const placeOrder = async (req,res) =>{
 }
 
 
-// Placing orders using Stripe method
+// Placing orders using Stripe method   need to create stripe account
 
 const placeOrderStripe = async (req,res) =>{
     try {
+        // const { userId, items, amount, address } = req.body
+        // const { origin } = req.headers;
+
+        // const orderData = {
+        //     userId,
+        //     items,
+        //     amount,
+        //     address,
+        //     paymentMethod:"Stripe",
+        //     payment: false,
+        //     date: Date.now()
+        // }
+        // const newOrder = new orderModel(orderData)
+        // await newOrder.save()
+
+        // const line_items = items.map((item) =>({
+        //     price_data: {
+        //         currency:currency,
+        //         product_data: {
+        //             name:item.name
+        //         },
+        //         unit_amount: item.price * 100
+        //     },
+        //     quantity: item.quantity
+        // }))
+
+        // line_items.push({
+        //     price_data: {
+        //         currency:currency,
+        //         product_data: {
+        //             name:'Deliver Charges'
+        //         },
+        //         unit_amount: deliveryCharge * 100
+        //     },
+        //     quantity: 1
+        // })
+
+        // const session = await stripe.checkout.session.create({
+        //     success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+        //     cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+        //     line_items,
+        //     mode: 'payment',
+        // })
+
+        // res.json({succes:true,session_url:session.url});
         
     } catch (error) {
         console.log(error)
     }
 }
+
+// Verify Stripe
+const verifyStripe = async (req,res) => {
+    const { orderId, success, userId} = req.body
+    try {
+        if (success === "true"){
+            await orderModel.findByIdAndUpdate(orderId, {payment:true});
+            await userModel.findByIdAndUpdate(userId, {cartData:{}})
+            res.json({success:true});
+
+        } else {
+            await orderModel.findByIdAndDelete(orderId)
+            res.json({success:false})
+        }
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+        
+    }
+}
+
+
+
+
+
 
 //  All Orders Data for Admin Panel...........
 
@@ -82,3 +163,4 @@ const updateStatus = async (req,res) =>{
 }
 
 export {placeOrder, placeOrderStripe, updateStatus, userOrders, allOrders}
+// Need to add verifyStripe
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import adminAuth from '../middleware/adminAuth.js'
 import { allOrders, placeOrder, placeOrderStripe, updateStatus, userOrders } from '../controllers/orderController.js'
+//Up Need to add verifyStripe
 import authUser from '../middleware/auth.js'
+import { verify } from 'jsonwebtoken'
 
 
 const orderRouter = express.Router()
@@ -16,6 +18,9 @@ orderRouter.post('/stripe',authUser, placeOrderStripe)
 
 // User Features
 orderRouter.post('/userorders',authUser, userOrders)
+
+// verify payment 
+// orderRouter.post('/verifyStripe',authUser,verifyStripe)
 
 
 export default orderRouter
